c3f4e2c5e948791b6ae657d3b6c1a2231b3c2437
Andreas Mathisen <mathisen90@msn.com>
2015-02-17 12:49:44 +0100

    Milestone 3

---
+++ Milestone 3/desktop/src/com/mygdx/milstone3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.milstone3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.milstone3.milestone3;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new milestone3(), config);
+	}
+}
