bfd09f4aeb5a33a55c2860eb4d857b21e8da072f
Andreas Mathisen <mathisen90@msn.com>
2015-02-17 14:17:21 +0100

    milestone 3

--- Milestone 2/core/src/com/mygdx/chessboard/chessboard.java
+++ Milestone 2/core/src/com/mygdx/chessboard/chessboard.java
@@ -90,24 +90,24 @@ public class chessboard extends ApplicationAdapter {
 				temp.put(position, 0, i, 0, j);
 				position++;
 				if((position%2) == 1 && j!=sizeZ-1 && i!=sizeX-1){ 
-					boxes.add(new ModelInstance(model, i+0.5f, 0, j+0.5f));
+					boxes.add(new ModelInstance(model, i+0.5f, 0.5f, j+0.5f));
 				}
 			}
 		}
-		/*Mat temp2 = Mat.zeros(sizeX*sizeZ, 1, CvType.CV_32FC3);
+		Mat temp2 = Mat.zeros(sizeX*sizeZ, 1, CvType.CV_32FC3);
 		int position2 = 0;
 		for(int j=0; j<sizeZ; j++){
 			for(int i=0; i<sizeX; i++){
 				temp2.put(position2, 0, i*26, j*26, 0);
 				position2++;
 			}
-		}*/
+		}
 		
 		boxInstance = new ModelInstance(model, 0, 0, 0);
 		
 		System.out.println(temp.dump());
 		objectPoints = new MatOfPoint3f(temp);
-		//objectPoints2 = new MatOfPoint3f(temp2);
+		objectPoints2 = new MatOfPoint3f(temp2);
 		System.out.println(objectPoints.dump());
 		
 		// Set up model batch
@@ -137,7 +137,7 @@ public class chessboard extends ApplicationAdapter {
 				imageSize = new Size(cameraFrame.size().width, cameraFrame.size().height);
 				Calib3d.solvePnP(objectPoints, corners, intrinsics, distortionCoefficients, rvec, tvec);
 				UtilAR.setCameraByRT(rvec, tvec, myCamera);
-				/*if(NotCalibrated){
+				if(NotCalibrated){
 					try{
 						Thread.sleep(500);
 					}
@@ -146,7 +146,7 @@ public class chessboard extends ApplicationAdapter {
 					tvecs.add(tvec);
 					cornerList.add(corners);
 					objectPointList.add(objectPoints2);
-				}*/
+				}
 				myCamera.update();
 				UtilAR.imDrawBackground(cameraFrame);
 				modelBatch.begin(myCamera);
@@ -157,12 +157,12 @@ public class chessboard extends ApplicationAdapter {
 				UtilAR.imDrawBackground(cameraFrame);
 			}
 		}
-		/*if(rvecs.size() > 49 && NotCalibrated){
+		if(rvecs.size() > 10 && NotCalibrated){
 			System.out.println("ObjectPointList "+objectPointList.size());
 			System.out.println("cornerList "+cornerList.size());
 			Calib3d.calibrateCamera(objectPointList, cornerList, imageSize, intrinsics, distortionCoefficients, rvecs, tvecs, Calib3d.CALIB_USE_INTRINSIC_GUESS);
 			NotCalibrated = false;
-		}*/	
+		}
 	}
 
 	
--- Milestone 3/core/src/com/mygdx/milstone3/milestone3.java
+++ Milestone 3/core/src/com/mygdx/milstone3/milestone3.java
@@ -1,29 +1,56 @@
 package com.mygdx.milstone3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.mygdx.milstone3.UtilAR;
 
 
 public class milestone3 extends ApplicationAdapter {
 	public Mat cameraFrame;
+	public Mat grayImage;
+	public Mat binaryImage;
+	public Mat blur;
 	public VideoCapture camera;
+	public List<MatOfPoint> contours;
+	public Mat hierarchy;
 	
 	@Override
 	public void create () {
 		camera = new VideoCapture(0);
 		camera.open(0);
 		cameraFrame = new Mat();
+		grayImage = new Mat();
+		binaryImage = new Mat();
+		blur = new Mat();
+		hierarchy = new Mat();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		if (camera.read(cameraFrame)) {			
+		if (camera.read(cameraFrame)) {
+			Imgproc.cvtColor(cameraFrame, grayImage, Imgproc.COLOR_BGR2GRAY);
+			Imgproc.GaussianBlur(grayImage, blur, new Size(5,5), 0);
+			//Imgproc.adaptiveThreshold(blur, binaryImage, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 2);			
+			Imgproc.threshold(blur, binaryImage, -1, 255, Imgproc.THRESH_BINARY+Imgproc.THRESH_OTSU);
+			contours = new ArrayList<MatOfPoint>();
+			
+			Imgproc.findContours(binaryImage, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+			System.out.println(contours.size());
+			Imgproc.drawContours(cameraFrame, contours, contours.size()-1, new Scalar(245, 7, 253));
 			UtilAR.imDrawBackground(cameraFrame);
-		}
+		}	
 	}
 }
